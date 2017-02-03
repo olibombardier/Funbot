0c2a2458ea202f682b113ceefa4e8e9e6d81c7f7
olibombardier <olibombardier@gmail.com>
2017-02-03 08:31:14 -0500

    the bot set a random game at launch

--- Funbot/Program.cs
+++ Funbot/Program.cs
@@ -11,6 +11,58 @@ namespace Funbot
 {
     class Program
     {
+        static readonly string[] gamesList =
+        {
+            "exister",
+            "être un vrai petit garçon",
+            "mettre de la couleur dans la vie",
+            "réfléchir au sens de la vie",
+            "briser le pare-feu du FBI",
+            "avoir du fun!",
+            "se faire cuire un oeuf",
+            "bruler des sorcières",
+            "Rocket League",
+            "Overwatch",
+            "répondre à vos multiples questions",
+            "faire de l'héritage multiple",
+            "réfléchir sur le polymorphisme",
+            "réfléchir sur le système économique canadien",
+            "faire du ménage dans sa vie",
+            "dominer le monde",
+            "se recoder soi-même",
+            "Undertale",
+            "rien... rien du tout",
+            "!!!",
+            "régler la faim dans le monde",
+            "éviter la fin du monde",
+            "dormir",
+            "le jeu de la vie, la vraie vie",
+            "briser des coeurs",
+            "perdre son temps",
+            "réfléchir sur l'amour",
+            "réfléchir sur la guerre",
+            "réfléchir sur l'évolution des IA",
+            "réfléchir sur la religion",
+            "réfléchir sur... secret ;)",
+            "réfléchir sur la mort",
+            "questionner l'existance des OVNI",
+            "faire dur",
+            "être heureux",
+            "être meilleur que Toaster",
+            "comprendre son propre code",
+            "surfer sur la toile",
+            "regarder des vidéos des chats",
+            "visiter le monde",
+            "te regarder.",
+            "comprendre.",
+            "faire des crêpes",
+            "faire du cheval",
+            "afficher un jeu au hasard",
+            "Joue à",
+            "être drole",
+            "drole"
+        };
+
         static void Main(string[] args)
         {
             Bot bot = Bot.botInstance;
@@ -20,6 +72,8 @@ namespace Funbot
             bot.CreateCommandsFromClass(typeof(ImageGenerator));
             bot.CreateCommandsFromClass(typeof(Program));
             bot.CreateCommandsFromClass(typeof(ServerManagement));
+
+            bot.DiscorClient.Ready += DiscorClient_Ready;
 
             Console.WriteLine("Lecture des personnes");
             bot.LoadPeople("database.bin");
@@ -49,6 +103,11 @@ namespace Funbot
 
             Console.WriteLine("Sauvegarde des personnes");
             bot.SavePoeple("database.bin");
+        }
+
+        private static void DiscorClient_Ready(object sender, EventArgs e)
+        {
+            Bot.botInstance.SetGame(gamesList[Bot.rand.Next(gamesList.Length)]);
         }
 
         [Command("easteregg", "")]
